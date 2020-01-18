21ad5a1b8bf287eabf868722ce45a004a34ae1e9
Craig Pinto <craigpinto17@gmail.com>
2020-01-18 16:56:46 -0500

    Created the base activity and class for the Query Results Page that will display recipe data

---
+++ app/src/main/java/com/example/uofthacksvii/ResultsPage.java
@@ -0,0 +1,76 @@
+package com.example.uofthacksvii;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class ResultsPage extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_page);
+
+        dl = (DrawerLayout)findViewById(R.id.activity_results_page);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.Home:
+                        Intent intent = new Intent(ResultsPage.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.Recommended:
+                        Intent intent1 = new Intent(ResultsPage.this, RecommendedPage.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.Nutrition:
+                        Intent intent2 = new Intent(ResultsPage.this, NutritionPage.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.Favourites:
+                        Intent intent3 = new Intent(ResultsPage.this, FavouritesPage.class);
+                        startActivity(intent3);
+                        break;
+
+                    default:
+                        return true;
+                }
+
+                return true;
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+}
