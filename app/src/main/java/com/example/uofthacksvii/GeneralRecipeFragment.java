d266653e94b073ea9e7c0e369580f017dbb5ca2a
Craig Pinto <craigpinto17@gmail.com>
2020-01-18 16:11:36 -0500

    Created the second Search Page with buttons where users can search for specific foods

--- app/src/main/java/com/example/uofthacksvii/GeneralRecipeFragment.java
+++ app/src/main/java/com/example/uofthacksvii/GeneralRecipeFragment.java
@@ -1,9 +1,12 @@
 package com.example.uofthacksvii;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +14,56 @@ import androidx.fragment.app.Fragment;
 
 public class GeneralRecipeFragment extends Fragment {
 
+
+    private ImageButton salad;
+    private ImageButton tacos;
+    private ImageButton burger;
+    private ImageButton noodles;
+
+    private SearchView search;
+    public String searchFood;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_general,container,false);
+
+        View v = inflater.inflate(R.layout.fragment_general,container,false);
+
+        salad = v.findViewById(R.id.salad);
+        salad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchFood = "salad";
+            }
+        });
+
+        tacos = v.findViewById(R.id.tacos);
+        tacos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchFood = "tacos";
+
+            }
+        });
+
+        burger = v.findViewById(R.id.burger);
+        burger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchFood = "burger";
+
+            }
+        });
+
+        noodles = v.findViewById(R.id.noodles);
+        noodles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchFood = "noodles";
+
+            }
+        });
+
+        return v;
     }
 }
